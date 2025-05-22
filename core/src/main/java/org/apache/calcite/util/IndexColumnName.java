e92383d098f7e7e79c8ab2265a2ea2fce73afdf9
Tao Wang <18234030530@163.com>
2025-05-22 18:49:47 +0800

    [fix][core] Index name ignores case when valiator

---
+++ core/src/main/java/org/apache/calcite/util/IndexColumnName.java
@@ -0,0 +1,7 @@
+package org.apache.calcite.util;
+
+public class IndexColumnName {
+
+    public static final String TEXT_INDEX_COLUMN = "$rank_bm25";
+    public static final String VECTOR_INDEX_COLUMN = "$distance";
+}
